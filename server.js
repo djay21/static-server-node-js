edf43a301afe742328b0e5d926f16e95679eccf2
Dheeraj kumar Gupta <47410636+djay21@users.noreply.github.com>
2020-12-04 16:53:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,26 @@
-var express = require('express');
-var app = express();
-var bp= require('body-Parser');
-var path = require('path');
- var dict =[];
- var c=0;
- app.use(bp.urlencoded({extended: true}));
- app.get('/login', function(req,res)
- {
-     res.sendFile(path.join(__dirname + '/login.html'));
-     console.log("listening at 8080");
+var express = require('express')
+var app = express()
+const fs = require("fs");
+const request = require('request');
+ 
+ 
+const port = 3000
+ 
+ 
 
- });
- app.post('/login',(req,res) =>
- { var user = req.body.user;
-    var pass = req.body.pass;
-    c=0;
-    if(dict.length==0)
-    {
-        dict.push({
-            key: user,
-            value:pass
-        });
-        res.end("ID Created");
-        console.log(dict);
-    }
- else
- {
-     for (var i=0;i<dict.length; i++)
-     {
-         if(dict[i].key==user)
-         {
-             res.end("id bn chuki h .. bnana mat");
-             c= 10;
-             console.log(dict);
-         }
-     }
-      if(c!=10){
-      dict.push({
-        key: user,value:pass    
-    });
-    res.end("ID Created");
-    console.log(dict);
-            }
-    } 
-}); app.listen(8080);
+ 
+   app.get('/chat.tar', function(req, res){
+      const file = `${__dirname}/chat.tar`;
+      console.log("h****************ITALIAN******************")
+      res.download(file); // Set disposition and send it.
+    });
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
